bfa515daf4f0317ea17ad2e4667647f15ecdd6b7
naotoooooo <ee213099@meiji.ac.jp>
2025-08-15 11:46:35 +0000

    perf：鳥瞰図修正

--- BEV.py
+++ BEV.py
@@ -1,3 +1,5 @@
+import os
+import glob
 import numpy as np
 import cv2
 
@@ -71,7 +73,8 @@ def birds_eye_equirectangular_corrected(equi_img,
 
 
     # # # v = (0.5 - lat / np.pi) * H # --- 重要部分：vを元の expected_H に合わせてマッピング ---
-    v = (0.5 - lat / np.pi) * expected_H - 160# 本来の2:1画像基準のv
+    # v = (0.5 - lat / np.pi) * expected_H - 145# 本来の2:1画像基準のv
+    v = (0.5 - lat / np.pi) * expected_H - 56  # 本来の2:1画像基準のv
 
     # # # しかし画像は H < expected_H → 範囲外を黒くする（cv2が自動でやる）
     # # v_full_clipped = v_full.copy()
@@ -82,19 +85,20 @@ def birds_eye_equirectangular_corrected(equi_img,
     # # 横方向uも通常通り
     u = (lon / (2 * np.pi) + 0.5) * W
     # # u = np.clip(u, 0, W - 1)  # u方向はトリミングされていないと仮定
-    # u = np.clip(u, 0, W - 1)  # u方向はトリミングされていないと仮定
+    u = np.clip(u, 0, W - 1)  # u方向はトリミングされていないと仮定
 
 
 
 
     # OpenCV remap用 float32マップ
-    map_x = u.astype(np.float32)
+    map_x = u.astype(np.float32) ###問題あり？
     map_y = v.astype(np.float32)
     
     
 
     # 出力鳥瞰ビュー画像
-    birds_eye = cv2.remap(equi_img, map_x, map_y, interpolation=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT, borderValue=(0, 0, 0))
+    # birds_eye = cv2.remap(equi_img, map_x, map_y, interpolation=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT, borderValue=(0, 0, 0))
+    birds_eye = cv2.remap(equi_img, map_x, map_y, interpolation=cv2.INTER_NEAREST, borderMode=cv2.BORDER_CONSTANT, borderValue=(0, 0, 0))   ##色がグラデーションにならない
     # birds_eye = cv2.flip(birds_eye, 1)  # 水平方向（左右）を反転
 
     
@@ -104,23 +108,53 @@ def birds_eye_equirectangular_corrected(equi_img,
     return birds_eye
 
 
-# img = cv2.imread("results_8noji_test/1740579295.965225.png_seg_only.png")
-# img = cv2.imread("results_8noji_test/1740578825.992192.png_seg_only.png")
-img = cv2.imread("datasets/outdoor_8noji_image_raw_2025-02-26-14-06-25/rgb/val/1740578825.992192.png")
-# img = cv2.imread("datasets/outdoor_8noji_image_raw_2025-02-26-14-06-25/rgb/val/1740579295.965225.png")
-# img = cv2.imread("results_8noji_test/1740579289.186688.png_seg_only.png")
+# # img = cv2.imread("results_8noji_test/1740579295.965225.png_seg_only.png")
+# # img = cv2.imread("results_8noji_test/1740578825.992192.png_seg_only.png")
+# img = cv2.imread("datasets/outdoor_8noji_image_raw_2025-02-26-14-06-25/rgb/val/1740578825.992192.png")
+# # img = cv2.imread("datasets/outdoor_8noji_image_raw_2025-02-26-14-06-25/rgb/val/1740579295.965225.png")
+# # img = cv2.imread("results_8noji_test/1740579289.186688.png_seg_only.png")
 
 
-bev = birds_eye_equirectangular_corrected(img,
-                                          camera_height=1.45,
-                                          grid_forward=40.0,
-                                          grid_side=20.0,
-                                          resolution=0.05)
+# bev = birds_eye_equirectangular_corrected(img,
+#                                           camera_height=1.45,
+#                                           grid_forward=40.0,
+#                                           grid_side=20.0,
+#                                           resolution=0.05)
 
-# cv2.imshow("Corrected Bird's Eye View", bev)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+# # cv2.imshow("Corrected Bird's Eye View", bev)
+# # cv2.waitKey(0)
+# # cv2.destroyAllWindows()
 
-# 保存
-cv2.imwrite("birds_eye_output_imgxxx.png", bev)
+# # 保存
+# cv2.imwrite("birds_eye_output_imgxxx.png", bev)
 
+input_dir = "results_outdoor_sii_intersection2"
+output_dir = "bev_results_outdoor_sii_intersection2"
+os.makedirs(output_dir, exist_ok=True)
+
+# 画像ファイル取得
+image_paths = sorted(glob.glob(os.path.join(input_dir, "*.png")))
+
+# すべての画像を処理
+for path in image_paths:
+    img = cv2.imread(path)
+    if img is None:
+        print(f"読み込み失敗: {path}")
+        continue
+
+
+    bev = birds_eye_equirectangular_corrected(img,
+                                            camera_height=1.46,
+                                            grid_forward=30.0,
+                                            grid_side=15,
+                                            resolution=0.05)
+
+    # cv2.imshow("Corrected Bird's Eye View", bev)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+
+    # 保存
+# cv2.imwrite("birds_eye_output_imgxxx4.png", bev)
+    basename = os.path.basename(path)
+    out_path = os.path.join(output_dir, f"bev_{basename}")
+    cv2.imwrite(out_path, bev)
